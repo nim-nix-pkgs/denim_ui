e229c23e8ce31ac53f04bb67413f15783d22eec8
hugosenari <hugosenari@gmail.com>
2022-05-12 11:50:58 -0300

    chore: re index denim_ui

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."denim_ui-master".dir   = "master";
+  inputs."denim_ui-master".owner = "nim-nix-pkgs";
+  inputs."denim_ui-master".ref   = "master";
+  inputs."denim_ui-master".repo  = "denim_ui";
+  inputs."denim_ui-master".type  = "github";
+  inputs."denim_ui-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."denim_ui-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
